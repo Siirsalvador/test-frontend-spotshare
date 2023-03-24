25ac9d621abbf980534c30d69b4392b96ac9f120
Adeola Adebayo <adeola.salvador@gmail.com>
2023-03-24 15:09:23 +0000

    minor style change

--- src/components/ListComponent.js
+++ src/components/ListComponent.js
@@ -22,10 +22,10 @@ function ListComponent(props) {
                     <li key={index}>
                         <ul>
                             <li>Played at: <span>{item.played_at}</span></li>
-                            <li>Track name:<span>{item.track_name}</span></li>
+                            <li>Track name: <span>{item.track_name}</span></li>
                             <li>Artist: <span>{item.track_artist}</span></li>
                             <li>Album name: <span>{item.track_album_name}</span></li>
-                            <li>Track link: <a href={item.track_link}>{item.track_link}</a></li>
+                            <li><a href={item.track_link}>Track link</a></li>
                         </ul>       
                     </li>
                     ))}
