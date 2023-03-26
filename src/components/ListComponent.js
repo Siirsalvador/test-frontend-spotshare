ec0386e28b124ed75a4e0c51fcd81aec2b6f0d98
Adeola Adebayo <adeola.salvador@gmail.com>
2023-03-26 18:20:18 +0100

    minor

--- src/components/ListComponent.js
+++ src/components/ListComponent.js
@@ -20,7 +20,6 @@ function ListComponent(props) {
     handleDataReceived(bundle);
     }, [bundle]);
 
-
     useEffect(() => {
         const handleOnlineStatusChange = () => {
             setIsOnline(navigator.onLine);
@@ -37,51 +36,51 @@ function ListComponent(props) {
 
     const handleDataReceived = (updatedData) => {
         setBundle(updatedData);
-      };
+    };
 
-      const handleClick = (event) => {
-        logEvent(analytics, "Next page click");
-        event.preventDefault();
-        fetch('https://spot-share.herokuapp.com/query', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ fromDate:bundle.fromDate, toDate:bundle.toDate })
-        })
-        .then(response => response.json())
-        .then(data => handleDataReceived({data:data, fromDate:bundle.fromDate, toDate:bundle.toDate}))
-        .catch(error => console.error(error));
-        window.scrollTo({
-            top: componentRef.current.offsetTop,
-            behavior: 'smooth'
-          });
-        };
+    const handleClick = (event) => {
+    logEvent(analytics, "Next page click");
+    event.preventDefault();
+    fetch('https://spot-share.herokuapp.com/query', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ fromDate:bundle.fromDate, toDate:bundle.toDate })
+    })
+    .then(response => response.json())
+    .then(data => handleDataReceived({data:data, fromDate:bundle.fromDate, toDate:bundle.toDate}))
+    .catch(error => console.error(error));
+    window.scrollTo({
+        top: componentRef.current.offsetTop,
+        behavior: 'smooth'
+        });
+    };
 
-        return (
-            <div className='container' ref={componentRef}>
-            {bundle.data.length > 0 ? (
-                <div>
-                <h1>Tracks:</h1>
-                <ul>
-                    {bundle.data.map((item, index) => (
-                    <li key={index}>
-                        <ul>
-                            <li>Played at: <span>{moment(item.played_at).format("dddd, MMMM Do, h:mm a")}</span></li>
-                            <li><span className='track-name'><a href={item.track_link} target="_blank" rel="noopener noreferrer">{item.track_name}</a></span></li>
-                            <li>Artist: <span>{item.track_artist}</span></li>
-                            <li>Album name: <span>{item.track_album_name}</span></li>
-                        </ul>       
-                    </li>
-                    ))}
-                </ul>
-                <span><button onClick={handleClick} disabled={bundle.data.length < 15 || !isOnline}>Next Page</button></span>
-                </div>
-            ) : (
-                <p>No data received yet</p>
-            )}
+    return (
+        <div className='container' ref={componentRef}>
+        {bundle.data.length > 0 ? (
+            <div>
+            <h1>Tracks:</h1>
+            <ul>
+                {bundle.data.map((item, index) => (
+                <li key={index}>
+                    <ul>
+                        <li>Played at: <span>{moment(item.played_at).format("dddd, MMMM Do, h:mm a")}</span></li>
+                        <li><span className='track-name'><a href={item.track_link} target="_blank" rel="noopener noreferrer">{item.track_name}</a></span></li>
+                        <li>Artist: <span>{item.track_artist}</span></li>
+                        <li>Album name: <span>{item.track_album_name}</span></li>
+                    </ul>       
+                </li>
+                ))}
+            </ul>
+            <span><button onClick={handleClick} disabled={bundle.data.length < 15 || !isOnline}>Next Page</button></span>
             </div>
-        );
+        ) : (
+            <p>No data received yet</p>
+        )}
+        </div>
+    );
 }
 
 export default ListComponent;
