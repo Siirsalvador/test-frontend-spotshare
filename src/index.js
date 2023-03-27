033c03c5448f814dac01401f42f2ea21ec9e54e8
Adeola Adebayo <adeola.salvador@gmail.com>
2023-03-27 13:53:38 +0100

    remove libs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import "mdb-react-ui-kit/dist/css/mdb.min.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./styles/index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
